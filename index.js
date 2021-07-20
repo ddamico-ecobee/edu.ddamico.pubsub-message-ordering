33a06f09e25e038118b9d7d7a908eefccd14bc1a
David D’Amico <david.damico@ecobee.com>
2021-07-20 15:02:04 -0400

    publish with orderingKey

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 const { PubSub } = require("@google-cloud/pubsub");
 const entries = require("./entries.json");
 
-const PROJECT_ID = `ordering`;
-const TOPIC_ID = `glucose-events`;
-const SUBSCRIPTION_ID = `glucose-events-sub`;
+const PROJECT_NAME = `ordering`;
+const TOPIC_NAME = `glucose-events`;
+const SUBSCRIPTION_NAME = `glucose-events-sub`;
 
-const pubsubSetup = () => {
+const MAX_MESSAGES = 10;
+const MAX_WAIT_TIME = 10;
+
+const pubsubSetup = async () => {
   // create topic, subscription
 };
 
-(async () => {
-  const pubsub = new PubSub({ PROJECT_ID });
+const pubsubCleanup = async (pubsub) => {
+  await pubsub.subscription(SUBSCRIPTION_NAME).delete();
+  await pubsub.topic(TOPIC_NAME).delete();
+};
 
-  const [topic] = await pubsub.createTopic(TOPIC_ID);
-  console.log(`Topic ${topic.name} created.`);
+(async () => {
+  const pubsub = new PubSub({ projectId: PROJECT_NAME });
+
+  try {
+    await pubsubCleanup(pubsub);
+  } catch (e) {
+    console.log("nothing to clean up");
+  }
+
+  const [topic] = await pubsub.createTopic(TOPIC_NAME);
+  //   const batchPublisher = pubsub.topic(TOPIC_NAME, {
+  //     batching: {
+  //       maxMessages: MAX_MESSAGES,
+  //       maxMilliseconds: MAX_WAIT_TIME * 1000,
+  //     },
+  //   });
+
+  const [subscription] = await pubsub
+    .topic(TOPIC_NAME)
+    .createSubscription(SUBSCRIPTION_NAME, { enableMessageOrdering: true });
+
+  subscription.on("message", (message) => {
+    console.log("Received message:", message.data.toString());
+  });
+
+  entries.forEach((entry) => {
+    const message = {
+      data: entry,
+      orderingKey: entry.mills,
+    };
+    const payload = Buffer.from(JSON.stringify(message));
+    // (async () => {
+    //   const messageId = await batchPublisher.publish(payload);
+    //   console.log(`Message ${messageId} published.`);
+    // })();
+    topic.publish(payload);
+  });
 })();
